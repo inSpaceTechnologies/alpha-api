79466d54aa374e837c4b1766ea8ba5bc975515ed
sgerami <sg@inspace.tv>
2018-05-01 12:14:16 +0100

    Connect to MongoDB using Mongoose.

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 
 const env = process.env.NODE_ENV;
 console.log("Envoroment: " + env);
 const config = require('./config')[env];
 
+const app = express();
+
 app.get('/', (req, res) => {
   res.send('Hello World.');
 });
 
-const port = config.server.port;
-app.listen(port, () => {
+
+
+mongoose.connect(config.mongo.uri);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+
+  console.log("Connected to database.");
+
+  const port = config.server.port;
+  app.listen(port, () => {
   console.log('Express listening on port ' + port + '.');
+  });
+
 });
