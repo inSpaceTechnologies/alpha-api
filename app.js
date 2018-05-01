abefb6c99d83568603dc363ae57ed7961f80960c
sgerami <sg@inspace.tv>
2018-05-01 15:28:48 +0100

    Use an error handler.

--- app.js
+++ app.js
@@ -7,8 +7,26 @@ const config = require('./config')[env];
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World.');
+function errorHandler(err, req, res, next) {
+  if (!err.status) {
+    err.status = 500;
+  }
+  if (err.log) {
+    console.error(err.message)
+  }
+  if (err.logStack) {
+    console.error(err.stack)
+  }
+  res.status(err.status).send(err.message);
+}
+
+app.get('/', (req, res, next) => {
+  var err = new Error("This is the error message");
+  err.status = 404;
+  err.log = true;
+  err.logStack = false;
+  next(err);
+  //res.send('Hello World.');
 });
 
 
@@ -21,9 +39,12 @@ db.once('open', function() {
 
   console.log("Connected to database.");
 
+  app.use(errorHandler);
+
+
   const port = config.server.port;
   app.listen(port, () => {
-  console.log('Express listening on port ' + port + '.');
+    console.log('Express listening on port ' + port + '.');
   });
 
 });
