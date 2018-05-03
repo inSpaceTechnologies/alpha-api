a8a7586d8d7ff65979be07d1b10b7e9eb6a0c5e9
sgerami <sg@inspace.tv>
2018-05-03 12:11:03 +0100

    Basic user schema and sign-up API.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const hash = require('pbkdf2-password')();
 
 const env = process.env.NODE_ENV;
 console.log("Envoroment: " + env);
@@ -9,7 +10,10 @@ const app = express();
 
 function errorHandler(err, req, res, next) {
   if (!err.status) {
-    err.status = 500;
+    err.status = 400;
+  }
+  if (!err.sendMessage) {
+    err.sendMessage = err.message;
   }
   if (err.log) {
     console.error(err.message)
@@ -17,30 +21,76 @@ function errorHandler(err, req, res, next) {
   if (err.logStack) {
     console.error(err.stack)
   }
-  res.status(err.status).send(err.message);
+  res.status(err.status).send(err.sendMessage);
 }
 
+function unknownError(err) {
+  err.sendMessage = "Unknown error";
+  err.logStack = true;
+  err.status = 500;
+  return err;
+}
+
+// so we don't have to use body-parser to get req.body
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (req, res, next) => {
-  var err = new Error("This is the error message");
-  err.status = 404;
-  err.log = true;
-  err.logStack = false;
-  next(err);
-  //res.send('Hello World.');
+  res.send('Hello World.');
 });
-
-
 
 mongoose.connect(config.mongo.uri);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+db.once('open', () => {
 
   console.log("Connected to database.");
 
-  app.use(errorHandler);
+  var userSchema = mongoose.Schema({
+    email: { type: String, unique: true },
+    salt: String,
+    passwordHash: String,
+  });
+  var User = mongoose.model('User', userSchema);
 
+  app.post('/signup', (req, res, next) => {
+
+    var email = req.body.email,
+        password = req.body.password;
+
+    if (!email) {
+      return next(new Error("You must supply an email address."));
+    }
+
+    User.findOne({ email: email}, (err, user) => {
+      if (err) {
+        return next(unknownError(err));
+      }
+      if (user) {
+        return next(new Error("Email address in use."));
+      }
+
+      hash({ password: password }, (err, pass, salt, passwordHash) => {
+        if (err) {
+          return next(unknownError(err));
+        }
+        // store the salt & hash in the database
+        var user = new User({
+          email: email,
+          salt: salt,
+          passwordHash: passwordHash
+        });
+        user.save((err, user) => {
+          if (err) {
+            return next(unknownError(err));
+          }
+          res.sendStatus(200);
+        });
+      });
+    });
+  });
+
+  app.use(errorHandler);
 
   const port = config.server.port;
   app.listen(port, () => {
