e37a14ac99dc26f1e739a6d1be6a6470e490c232
sgerami <sg@inspace.tv>
2018-05-16 17:42:22 +0100

    Add linter (ESLint) and npm script.

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "extends": [
+    "airbnb-base",
+  ],
+  "rules": {
+    "no-param-reassign": [2, { "props": false }],
+    'max-len': ['off'],
+    'no-console': ['off'],
+  },
+  "env": {
+    "node": true,
+  },
+};
